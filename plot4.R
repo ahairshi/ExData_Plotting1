a44af2eae051d34261b29d9edc1f4414035c83c8
ahairshi <daisyfuentesahamed@gmail.com>
2016-06-14 05:38:06 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -8,14 +8,19 @@ dev.cur()
 
 par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 
+#Plot 1
 with(consumption,plot(datetime, Global_active_power, type = "l", ylab = "Global Active Power", xlab = ""))
+
+#Plot 2
 with(consumption,plot(datetime, Voltage, type = "l"))
 
+#Plot 3
 with(consumption, plot(datetime, Sub_metering_1,xlab = "", ylab = "Energy sub metering",type = "l"))
 with(consumption, points(datetime, Sub_metering_2, col = "red", type = "l"))
 with(consumption, points(datetime, Sub_metering_3, col = "blue", type = "l"))
 legend("topright",  lty = 1, lwd = 2,bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+#Plot 4
 with(consumption,plot(datetime, Global_reactive_power, type = "l"))
 
 dev.off()
